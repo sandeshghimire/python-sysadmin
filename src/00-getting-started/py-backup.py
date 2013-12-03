a79d09e9ae7a1e20bdb94b6cf5365e5392349c72
sandesh ghimire <sandeshghimire@outlook.com>
2013-12-03 09:48:02 -0800

    adding py-backup

---
+++ src/00-getting-started/py-backup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python 
+
+import syslog
+
+class PyBackup(object):
+    def __init__ (self, server_name, username, password):
+        syslog.syslog("starting backup process")
+        self.server_name= server_name
+        self.username = username
+        self.password = password
+        
+    
+    
